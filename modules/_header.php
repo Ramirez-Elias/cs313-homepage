4c2b98ccc2ecdc43fe1bbe23a56ea61f6c35994e
Ramirez-Elias <ram16032@byui.edu>
2019-05-07 21:41:26 -0500

    changed the name of the logo image

--- modules/_header.php
+++ modules/_header.php
@@ -1,6 +1,6 @@
   <body>
     <header>
-        <img src="images/logo.jpg" alt="The site logo" width="150">
+        <img src="images/Logo.jpg" alt="The site logo" width="150">
         <h1>Elias Ramirez</h1>
         <div class="subheading">CS313 Homepage</div>    
     </header>
